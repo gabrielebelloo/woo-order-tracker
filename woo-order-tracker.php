0c7d41abf74b49a1180096a104641cb58434700f
Gabriele <briello2001@gmail.com>
2024-12-17 12:27:08 +0100

    First commit

---
+++ woo-order-tracker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package WooOrderTrackerPlugin
+ */
+/*
+Plugin Name: Woo Order Tracker Plugin
+Plugin URI: https://github.com/gabrielebelloo
+Description: This is a Plugin for Wordpress and WooCommerce capable of tracking orders.
+Version: 1.0.0
+Author: Gabriele Bello
+Author URI: https://www.linkedin.com/in/gabrielebello/
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: woo-order-tracker
+*/
