fae7adfa0878cc8029973aebda47e2a934844e45
Gabriele <briello2001@gmail.com>
2024-12-22 14:42:12 +0100

    Improve webhook handling and error logging

--- src/Base/WooWebhook.php
+++ src/Base/WooWebhook.php
@@ -24,17 +24,21 @@ class WooWebhook {
   public function send_order_webhook( $order_id ) {
     $called_hook = current_filter();
 
-    if ( $called_hook == 'woocommerce_order_note_added' ) {
-      $note = get_comment( $order_id );
-      $order_id = $note->comment_post_ID;
-    }
-
-    error_log( $order_id );
-
     $settings = get_option( 'woo_settings' );
 
     if ( isset( $settings['order_tracking'] ) && !$this->called_hooks ) {
       $webhook_url = $settings['webhook_url'];
+
+      if ( $called_hook == 'woocommerce_order_note_added' ) {
+        $note = get_comment( $order_id );
+        if ( $note && isset( $note->comment_post_ID ) ) {
+          $order_id = $note->comment_post_ID;
+        } else {
+          error_log( "Invalid note ID or comment not found: $order_id" );
+          return;
+        }
+      }
+
       $order = wc_get_order( $order_id ); 
 
       if ( !$order ) {
@@ -42,9 +46,12 @@ class WooWebhook {
         return;
       }
 
-      $order_data = $order->get_data();
+      if ( !$settings['webhook_url'] ) {
+        error_log( "Webhook URL not set in settings." );
+        return;
+      }
 
-      error_log( $webhook_url );
+      $order_data = $order->get_data();
 
       $response = wp_remote_post($webhook_url, array(
         'method' => 'POST',
@@ -54,7 +61,11 @@ class WooWebhook {
         )
       ));
 
-      error_log( json_encode( $response ) );
+      if ( is_wp_error( $response ) ) {
+        error_log( $response->get_error_message() );
+      } else {
+        error_log( $response['body'] );
+      }
     }
 
     array_push( $this->called_hooks, current_filter() );
