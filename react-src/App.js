d7e1e5a34e98b1df2160f0a29dae43f5b6225203
Gabriele <briello2001@gmail.com>
2024-12-23 12:57:15 +0100

    Add react-hot-toast for notifications

--- react-src/App.js
+++ react-src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import SettingsForm from './components/SettingsForm';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return <>
+    <div><Toaster
+      position="top-right"
+      reverseOrder={false}
+      toastOptions={{
+        style: {
+          marginTop: '25px',
+        },
+      }}
+    /></div>
     <SettingsForm />
   </>;
 }
--- react-src/components/SettingsForm.jsx
+++ react-src/components/SettingsForm.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
 
 const SettingsForm = () => {
   const [orderTracking, setOrderTracking] = useState(wooOrderTrackerSettings.order_tracking);
   const [webhookUrl, setWebhookUrl] = useState(wooOrderTrackerSettings.webhook_url);
-
-  useEffect(() => {
-    console.log(wooOrderTrackerSettings);
-  });
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setLoading(true);
 
     const formData = new URLSearchParams();
     formData.append('action', 'save_settings');
@@ -24,18 +24,18 @@ const SettingsForm = () => {
         'Content-Type': 'application/x-www-form-urlencoded',
       }
     })
-    .then(response => response.json())
+    .then(response => {response.json(); setLoading(false); toast.success("Settings saved successfully")}) 
     .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    .catch(error => {console.error('Error:', error); setLoading(false); toast.error("Error saving settings")});
   }
 
   return <div>
     <h2>Woo Order Tracker Settings</h2>
 
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} disabled={loading}>
       <label className='order-tracking'>  
         <div>Enable tracking</div>
-        <select value={orderTracking} onChange={(e) => setOrderTracking(e.target.value)}>
+        <select value={orderTracking} onChange={(e) => setOrderTracking(e.target.value)} disabled={loading}>
           <option value='1'>Actived</option>
           <option value='0'>Deactivated</option>
         </select>
@@ -43,11 +43,17 @@ const SettingsForm = () => {
 
       <label className='webhook-url'>
         <div>Webhook URL</div>
-        <input type="text" value={webhookUrl} placeholder='https://your-webhook-url.com' onChange={(e) => setWebhookUrl(e.target.value)} />
+        <input type="text" value={webhookUrl} placeholder='https://your-webhook-url.com' onChange={(e) => setWebhookUrl(e.target.value)} disabled={loading}/>
       </label>
 
-      <button type='submit'>Save</button>
-    </form>
+      <button 
+          type='submit' 
+          disabled={loading}
+          style={{ opacity: loading ? 0.5 : 1 }}
+        >
+          {loading ? 'Saving...' : 'Save'}
+        </button>
+    </form> 
   </div>
 }
 
